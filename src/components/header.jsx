8f82047fe4a8c235133b0fe243a8d8b143229d65
kazuya-m <kazuya.matsuo5@gmail.com>
2021-03-27 12:34:25 +0900

    modify header style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,16 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className="hover:underline">Blog</a>
-      </Link>
-      .
-    </h2>
+    <header className="h-1/4 flex flex-row justify-between">
+      <h1 className="text-4xl md:text-5xl xl:text-6xl px-8 py-7 font-bold">
+        <Link href="/">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className="cursor-pointer">
+            <span className="text-red-600">DIA</span>LOG
+          </a>
+        </Link>
+      </h1>
+      <div className="px-7 py-7">Twitter</div>
+    </header>
   )
 }
