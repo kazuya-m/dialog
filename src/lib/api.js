e17c74212ee494ada199e6a1edd6f10c574fbd9d
kazuya-m <kazuya.matsuo5@gmail.com>
2021-04-30 09:42:58 +0900

    delete apis

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,47 +1,47 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import { join } from 'path'
+// import fs from 'fs'
+// import matter from 'gray-matter'
+// import { join } from 'path'
 
-const postsDirectory = join(process.cwd(), '_posts')
+// const postsDirectory = join(process.cwd(), '_posts')
 
-export const getPostSlugs = () => {
-  return fs.readdirSync(postsDirectory)
-}
+// export const getPostSlugs = () => {
+//   return fs.readdirSync(postsDirectory)
+// }
 
-export const getPostBySlug = (slug, fields = []) => {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
+// export const getPostBySlug = (slug, fields = []) => {
+//   const realSlug = slug.replace(/\.md$/, '')
+//   const fullPath = join(postsDirectory, `${realSlug}.md`)
+//   const fileContents = fs.readFileSync(fullPath, 'utf8')
+//   const { data, content } = matter(fileContents)
 
-  const items = {}
+//   const items = {}
 
-  // Ensure only the minimal needed data is exposed
-  fields.forEach((field) => {
-    if (field === 'slug') {
-      items[field] = realSlug
-    }
-    if (field === 'content') {
-      items[field] = content
-    }
+//   // Ensure only the minimal needed data is exposed
+//   fields.forEach((field) => {
+//     if (field === 'slug') {
+//       items[field] = realSlug
+//     }
+//     if (field === 'content') {
+//       items[field] = content
+//     }
 
-    if (data[field]) {
-      items[field] = data[field]
-    }
-  })
+//     if (data[field]) {
+//       items[field] = data[field]
+//     }
+//   })
 
-  return items
-}
+//   return items
+// }
 
-export const getAllPosts = (fields = []) => {
-  const slugs = getPostSlugs()
-  const posts = slugs
-    .map((slug) => {
-      return getPostBySlug(slug, fields)
-    })
-    // sort posts by date in descending order
-    .sort((post1, post2) => {
-      return post1.date > post2.date ? -1 : 1
-    })
-  return posts
-}
+// export const getAllPosts = (fields = []) => {
+//   const slugs = getPostSlugs()
+//   const posts = slugs
+//     .map((slug) => {
+//       return getPostBySlug(slug, fields)
+//     })
+//     // sort posts by date in descending order
+//     .sort((post1, post2) => {
+//       return post1.date > post2.date ? -1 : 1
+//     })
+//   return posts
+// }
