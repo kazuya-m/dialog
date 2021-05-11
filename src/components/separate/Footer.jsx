6ae8cf693264e799891b98c61a6bd7705c0136c0
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-11 11:04:08 +0900

    modify footer

--- src/components/separate/Footer.jsx
+++ src/components/separate/Footer.jsx
@@ -2,9 +2,9 @@ import { Container } from '../shared/Container'
 
 export const Footer = () => {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="bg-accent-1 border-t border-accent-2 py-5">
       <Container>
-        <h1>Footer</h1>
+        <p className="text-center text-black-300">DIALOG All Rights Reserved.</p>
       </Container>
     </footer>
   )
