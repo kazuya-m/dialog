079ffeb085a5b0b2d46a9b0b2ab741b658df6c29
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-12 18:52:05 +0900

    add index path

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,18 +7,26 @@ export const Pagination = ({ path, totalCount }) => {
   const router = useRouter()
   const pathArray = router.asPath.split('/')
   const currentPageNumber = pathArray[pathArray.length - 1]
+  const isIndex = router.asPath === '/'
+
   const pageAmount = getPageAmount(1, totalCount)
   return (
-    <ul className="flex justify-center">
+    <ul className="flex justify-center mb-4" key={currentPageNumber}>
       {pageAmount.map((number) => {
         return (
           <Link href={`/${path}/${number}`}>
-            {currentPageNumber === number.toString() ? (
-              <li key={number} className="mx-1 mb-3 px-2 border border-red-500 text-white bg-red-500 rounded-md">
+            {currentPageNumber === number.toString() || (isIndex && number === 1) ? (
+              <li
+                key={number}
+                className="mx-1 mb-3 px-2 border border-red-500 text-white bg-red-500 rounded-md cursor-pointer"
+              >
                 {number}
               </li>
             ) : (
-              <li key={number} className="mx-1 mb-3 px-2 border border-red-500 text-red-500 rounded-md">
+              <li
+                key={number}
+                className="mx-1 mb-3 px-2 border border-red-100 border-opacity-25 text-red-500 bg-red-100 bg-opacity-25 rounded-md cursor-pointer"
+              >
                 {number}
               </li>
             )}
