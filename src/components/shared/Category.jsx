0c22c04aa612a8379312046823a057fb0253c855
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-13 21:33:11 +0900

    ♻️

--- src/components/shared/Category.jsx
+++ src/components/shared/Category.jsx
@@ -3,15 +3,13 @@ import Link from 'next/link'
 
 export const Category = ({ category }) => {
   return (
-    <Link as={`/${category.id}/1`} href="/[categoryId]/[number]">
-      <a className="text-black-400">
-        <div className="flex items-center pl-1 my-4">
+    <>
+      <Link as={`/${category.id}/1`} href="/[categoryId]/[number]">
+        <a className="flex items-center pl-1.5 my-4 text-black-400">
           <FolderIcon className="w-4 h-4 mr-0.5" />
-          <button className="text-xs" type="button">
-            {category.name}
-          </button>
-        </div>
-      </a>
-    </Link>
+          <p className="text-xs">{category.name}</p>
+        </a>
+      </Link>
+    </>
   )
 }
