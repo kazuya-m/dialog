5b26d4771a060b090a1c39f360a96b604132f8d5
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-13 19:12:33 +0900

    ♻️

--- src/lib/microcms/api.js
+++ src/lib/microcms/api.js
@@ -5,21 +5,26 @@ const key = {
   headers: { 'X-API-KEY': process.env.API_KEY },
 }
 
-// 全ての記事を取得
-export const getAllPosts = async () => {
-  const allPosts = await fetch(process.env.GET_POSTS_API, key)
+// 記事詳細取得時に取得する要素
+const postFields = 'id,publishedAt,title,body,category.id,category.name,author.name,author.icon.url,thumbnail.url'
+// 記事一覧取得時に取得する要素
+const postPreviewFields = 'id,publishedAt,title,category.id,category.name,author.name,author.icon.url,thumbnail.url'
+
+// 全記事の数を取得
+export const getAllPostsTotalCount = async () => {
+  const response = await fetch(`${process.env.GET_POSTS_API}?limit=0&fields=totalCount`, key)
     .then((res) => {
       return res.json()
     })
     .catch(() => {
       return null
     })
-  return allPosts
+  return response.totalCount
 }
 
-// IDで記事を取得
+// 記事詳細を取得
 export const getPostById = async (id) => {
-  const post = await fetch(`${process.env.GET_POSTS_API}/${id}`, key)
+  const post = await fetch(`${process.env.GET_POSTS_API}/${id}?fields=${postFields}&depth=1`, key)
     .then((res) => {
       return res.json()
     })
@@ -29,25 +34,57 @@ export const getPostById = async (id) => {
   return post
 }
 
-// ページ分の記事を取得
+// 1ページ分の記事を取得
 export const getPostsPerPage = async (offset) => {
-  const posts = await fetch(`${process.env.GET_POSTS_API}?offset=${offset * PER_PAGE}&limit=${PER_PAGE}`, key)
+  const offsetNumber = Number(offset)
+  // 0(index)の場合は0
+  const offsetPerPage = offsetNumber !== 0 ? (offsetNumber - 1) * PER_PAGE : 0
+  const posts = await fetch(
+    `${process.env.GET_POSTS_API}?offset=${offsetPerPage}&limit=${PER_PAGE}&fields=${postPreviewFields}`,
+    key,
+  )
+    .then((res) => {
+      return res.json()
+    })
+    .catch((err) => {
+      return null
+    })
+  return posts
+}
+
+// カテゴリ毎の全記事数を取得
+export const getPostsCountCategories = async (categoryId) => {
+  const response = await fetch(
+    `${process.env.GET_POSTS_API}?filters=category=${categoryId}&limit=0&fields=totalCount`,
+    key,
+  )
     .then((res) => {
       return res.json()
     })
     .catch(() => {
       return null
     })
-
-  return posts
+  return response.totalCount
 }
 
-// カテゴリ毎かつページ分の記事を取得
+// 全てのカテゴリを取得(idと名称) カテゴリ一覧ページ実装時に使う
+// export const getAllCategories = async () => {
+//   const allCategories = fetch(`${process.env.GET_CATEGORIES_API}?limit=100&?fields=id,name`, key)
+//     .then((res) => {
+//       return res.json()
+//     })
+//     .catch(() => {
+//       return null
+//     })
+//   return allCategories
+// }
+
+// カテゴリ毎かつ1ページ分の記事を取得
 export const getPostsByCategoryPerPage = async (categoryId, offset) => {
+  const offsetNumber = Number(offset)
+  const offsetPerPage = offsetNumber !== 0 ? (offsetNumber - 1) * PER_PAGE : 0
   const posts = await fetch(
-    `${process.env.GET_POSTS_API}?filters=category[equals]${categoryId}[and]offset=${
-      offset * PER_PAGE
-    }&limit=${PER_PAGE}`,
+    `${process.env.GET_POSTS_API}?filters=category[equals]${categoryId}&offset=${offsetPerPage}&limit=${PER_PAGE}&fields=${postPreviewFields}`,
     key,
   )
     .then((res) => {
@@ -58,16 +95,4 @@ export const getPostsByCategoryPerPage = async (categoryId, offset) => {
     })
 
   return posts
-}
-
-// 全てのカテゴリを取得
-export const getAllCategories = async () => {
-  const allCategories = fetch(process.env.GET_CATEGORIES_API, key)
-    .then((res) => {
-      return res.json()
-    })
-    .catch(() => {
-      return null
-    })
-  return allCategories
 }
