2c2ee05473040fc31f1a5ec266ece76ca560e101
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-15 15:14:16 +0900

    ✨ add date distance formatter

--- src/components/utils/date/Date-formatter.jsx
+++
@@ -1,7 +0,0 @@
-import { formatDistance, parseISO, subDays } from 'date-fns'
-
-export const DateFormatter = ({ dateString }) => {
-  const date = parseISO(dateString)
-  // return <time dateTime={dateString}>{format(date, 'yyyy.LL.dd')}</time>
-  return formatDistance(new Date(), date, { addSuffix: true })
-}
