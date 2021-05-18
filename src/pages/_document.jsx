15a04dffbc6ee931ca85708cd1bb25dd9bac5802
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-18 22:51:12 +0900

    📈

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,31 @@
+/* eslint-disable react/no-danger */
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { existsGaId, GA_ID } from 'src/lib/gtag'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {existsGaId && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
