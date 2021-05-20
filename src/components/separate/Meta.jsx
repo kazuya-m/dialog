5eb104e1d9ccf8174f173079efbae431670c58f0
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-20 22:12:40 +0900

    🐛

--- src/components/separate/Meta.jsx
+++ src/components/separate/Meta.jsx
@@ -7,7 +7,7 @@ export const Meta = () => {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
