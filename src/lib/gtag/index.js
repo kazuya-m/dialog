c50c17c0185c13b27476ee3bc6dcfd5f8b746ed9
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-24 09:37:33 +0900

    📈

--- src/lib/gtag/index.js
+++ src/lib/gtag/index.js
@@ -1,11 +1,16 @@
 export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+export const UA_ID = process.env.NEXT_PUBLIC_GOOGLE_UNIVERSAL_ANALYTICS_ID
 
 // IDが取得できない場合を想定する
 export const existsGaId = GA_ID !== ''
+export const existsUaId = UA_ID !== ''
 
 // PVを測定する
 export const pageView = (path) => {
   window.gtag('config', GA_ID, {
+    page_path: path,
+  })
+  window.gtag('config', UA_ID, {
     page_path: path,
   })
 }
