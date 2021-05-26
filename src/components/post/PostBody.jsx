18703d34e885f13b6f5e2045c5ca209c43db8c7d
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-26 22:22:49 +0900

    💄

--- src/components/post/PostBody.jsx
+++ src/components/post/PostBody.jsx
@@ -4,7 +4,13 @@ export const PostBody = ({ content }) => {
   return (
     <>
       {/* eslint-disable-next-line react/no-danger */}
-      <div className={postStyles.post} dangerouslySetInnerHTML={{ __html: content }} />
+      <div className={`${postStyles.post} post-body`} dangerouslySetInnerHTML={{ __html: content }} />
+      <style jsx global>{`
+        .post-body a {
+          color: #e7002b;
+          opacity: 0.8;
+        }
+      `}</style>
     </>
   )
 }
