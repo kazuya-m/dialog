8b41ef967a49c4476d3ba913ddc7f73381b770c1
kazuya-m <kazuya.matsuo5@gmail.com>
2021-05-28 22:02:08 +0900

    ♻️

--- src/components/post/Reprinting.jsx
+++ src/components/post/Reprinting.jsx
@@ -4,8 +4,8 @@ import { TWITTER_URL } from 'src/constants'
 export const Reprinting = ({ author }) => {
   return (
     <div className="flex items-center px-4 py-6 bg-yellow-100 bg-opacity-70">
-      <div className="w-20 text-yellow-300">
-        <ExclamationCircleIcon className="w-10 h-10" />
+      <div className="w-20">
+        <ExclamationCircleIcon className="w-10 h-10 text-yellow-300" />
       </div>
       <div className="text-sm leading-relaxed tracking-wide md:text-base">
         <p>
