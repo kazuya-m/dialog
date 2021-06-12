a30b8d8d1336e3f8558d466fd59c8dd7b6dae919
kazuya-m <kazuya.matsuo5@gmail.com>
2021-06-12 15:17:15 +0900

    🍱

--- src/components/separate/Header.tsx
+++ src/components/separate/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { SectionSeparator } from '../utils/separator/SectionSeparator'
@@ -6,15 +7,11 @@ export const Header = () => {
   return (
     <header className="text-center h-1/5">
       <div className="flex flex-col justify-center py-4">
-        <p className="text-4xl font-bold md:text-5xl xl:text-6xl">
-          <Link href="/">
-            <a className="cursor-pointer">
-              <span className="text-red-500">DIA</span>
-              <span className="text-black-900">LOG</span>
-            </a>
-          </Link>
-          <p className="text-xs text-black-300 mt-0.5 font-light">浦和レッズをもっとサポートするためのコミュニティ</p>
-        </p>
+        <Link href="/">
+          <a className="cursor-pointer">
+            <Image src="/assets/logo-w-mantra.svg" width={250} height={90} />
+          </a>
+        </Link>
       </div>
       <SectionSeparator />
     </header>
